eec06d8d7e025eb90be653f4e2a11a85c7919619
qingen <290142650@qq.com>
2019-07-25 01:00:24 +0800

    Swift

--- HuiXiangShou/ViewController.swift
+++ HuiXiangShou/ViewController.swift
@@ -7,14 +7,25 @@
 //
 
 import UIKit
-
+import RxSwift
 class ViewController: UIViewController {
-
+    fileprivate lazy var bag = DisposeBag()
     override func viewDidLoad() {
         super.viewDidLoad()
+        let button: UIButton = UIButton.init(type: .custom)
+        button.addTarget(self, action: #selector(btn1Click(btn:)), for: .touchUpInside)
+        button.backgroundColor = UIColor.red
+        button.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        button.center = self.view.center
+        self.view.addSubview(button)
+        
+    
         // Do any additional setup after loading the view.
     }
 
-
+    @objc func btn1Click(btn: UIButton) -> Void {
+        print("shishikan")
+    }
+   
 }
 
